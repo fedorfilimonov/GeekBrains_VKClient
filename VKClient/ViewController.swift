e2013e09ae03cdd4e062cd045aa6eedc232f6ec7
Fedor Filimonov <f.filimonov@gmail.com>
2020-07-02 14:05:28 +0300

    Initial Commit

---
+++ VKClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKClient
+//
+//  Created by Федор Филимонов on 02.07.2020.
+//  Copyright © 2020 fedorfilimonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
